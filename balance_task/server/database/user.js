207129e66a5f328be8bead2bd58e3a02ccfb4be0
백정훈 <bjh9807@naver.com>
2021-07-18 21:01:09 +0900

    start

---
+++ balance_task/server/database/user.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+const usersSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  : {
+    type: String,
+    required: true,
+    unique: true
+  },
+  thumbnailUrl: {
+    type: String
+  },
+  category: {
+    type: String
+  },
+  price: {
+    type: Number
+  }
+});
+
+module.exports = mongoose.model("users", goodsSchema);
