c2f779926b4a7dc4ebd90bead8dfba8c00100e6b
하동호 <cubix101@naver.com>
2021-08-05 22:26:59 +0900

    정탈꺼 받음

--- balance_task/server/mysql1.js
+++ balance_task/server/mysql1.js
@@ -24,13 +24,6 @@ const saltRounds = 10;
 var mysql = require("mysql");
 
 //======패스포드아용===//
-<<<<<<< HEAD
-var passport = require("passport");
-var flash = require("connect-flash");
-var localStrategy = require("passport-local").Strategy;
-var KakaoStrategy = require("passport-kakao").Strategy;
-//var flash=require('flash');
-=======
 var passport =require('passport');
 var localStrategy=require('passport-local').Strategy;
 var KakaoStrategy=require('passport-kakao').Strategy;
@@ -43,7 +36,6 @@ const cookie = require('cookie');
 
 
 
->>>>>>> 22e642b8415732715978e77728ecdb1b7e32deec
 
 //===== MySQL 데이터베이스 연결 설정 =====//
 var pool = mysql.createPool({
@@ -74,85 +66,6 @@ app.use("/public", static(path.join(__dirname, "public")));
 app.use(cookieParser());
 
 // 세션 설정
-<<<<<<< HEAD
-app.use(
-  expressSession({
-    secret: "my key",
-    resave: true,
-    saveUninitialized: true,
-  })
-);
-
-var router = express.Router();
-// 카카오 로그인 세션
-passport.use(
-  "kakao-login",
-  new KakaoStrategy(
-    {
-      clientID: "6e9775355a9f75a716cfc8153f2ff2eb",
-      callbackURL: "http://localhost:3000/kakao/oauth",
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken);
-      console.log(profile);
-    }
-  )
-);
-
-app.get("/kakao", passport.authenticate("kakao-login"));
-
-app.get(
-  "/kakao/callback",
-  passport.authenticate("kakao-login", { failureRedirect: "/" }),
-  (req, res) => {
-    res.redirect("/");
-  }
-);
-
-//module.exports = router;
-
-//passport 초기화
-// app.use(passport.initialize());
-// app.use(passport.session());
-// app.use(flash());
-
-//passport strategy
-// var localStrategy=require('passport-local').Strategy;
-// passport.use('local-login',new localStrategy({
-//     usernameField:'email',
-//     passwordField:'password',
-//     passReqToCallback:true
-
-// },function(req,email,password,done){
-//     console.log('passport의 로그인이 호출됨:'+email+','+pasword);
-
-//     var database=app.get('database');
-//     database.UserModel.fin
-// }));
-
-// app.post('/Signup',
-//     passport.authenticate('local',{
-//         successRedirect:'/',
-//         failureRedirect:'/Signup',
-//         failureFlash:true
-//     }));
-
-// passport.use(new localStrategy(
-//     function(username, password, done){
-//         User.findOnd({username: username},function(err, user){
-//             if(err){return done(err);}
-//             if(!user){
-//                 return done(null,false, req.flash('loginMessage','등록된 계정이 없습니다.'));
-//             }
-//             if(!user.validPassword(password)){
-//                 return done(null,false, req.flash('loginMessage','비밀번호가 다릅니다.'))
-//             }
-
-//             return done(null,user);
-//         })
-//     }
-// ))
-=======
 app.use(expressSession({
 	secret:'my key',
 	resave:true,
@@ -194,7 +107,6 @@ router.get(
     failureRedirect: "/api/auth/fail"
   })
 );
->>>>>>> 22e642b8415732715978e77728ecdb1b7e32deec
 
 //==패스포트==//
 
@@ -210,64 +122,6 @@ router.route("/api/signup").post(function (req, res) {
   var paramName = req.body.name || req.query.name;
   var paramAgreement = req.body.isCheck || req.query.isCheck;
 
-<<<<<<< HEAD
-  const encryptedPassowrd = bcrypt.hashSync(paramPassword, 10);
-
-  if (pool) {
-    addUser(
-      paramId,
-      paramName,
-      encryptedPassowrd,
-      paramAgreement,
-      function (err, addedUser) {
-        // 동일한 id로 추가하려는 경우 에러 발생 - 클라이언트로 에러 전송
-        if (err) {
-          console.error("사용자 추가 중 에러 발생 : " + err.stack);
-
-          res.writeHead("200", { "Content-Type": "text/html;charset=utf8" });
-          res.write("<h2>사용자 추가 중 에러 발생</h2>");
-          res.write("<p>" + err.stack + "</p>");
-          res.end();
-
-          return;
-        }
-
-        // 결과 객체 있으면 성공 응답 전송
-        if (addedUser) {
-          console.dir(addedUser);
-
-          var insertId = addUser.insertId;
-          console.log("추가한 레코드의 아이디 : " + insertId);
-
-          res.writeHead("200", { "Content-Type": "text/html;charset=utf8" });
-          res.write("<h2>사용자 추가 성공</h2>");
-          res.end();
-        } else {
-          res.writeHead("200", { "Content-Type": "text/html;charset=utf8" });
-          res.write("<h2>사용자 추가  실패</h2>");
-          res.end();
-        }
-      }
-    );
-  } else {
-    // 데이터베이스 객체가 초기화되지 않은 경우 실패 응답 전송
-    res.writeHead("200", { "Content-Type": "text/html;charset=utf8" });
-    res.write("<h2>데이터베이스 연결 실패</h2>");
-    res.end();
-  }
-  console.log(
-    "요청 파라미터 : " +
-      paramId +
-      ", " +
-      paramPassword +
-      ", " +
-      paramName +
-      ", " +
-      paramAgreement
-  );
-
-  // pool 객체가 초기화된 경우, addUser 함수 호출하여 사용자 추가
-=======
 router.route('/api/signup').post(function(req, res) {
 	console.log('/process/adduser 호출됨.');
     
@@ -371,7 +225,6 @@ app.post("/api/user/login", (req, res) => {
       }
     }
   });
->>>>>>> 22e642b8415732715978e77728ecdb1b7e32deec
 });
 
 // 라우터 객체 등록
@@ -404,8 +257,6 @@ var addUser = function (id, name, password, agreement, callback) {
         conn.release(); // 반드시 해제해야 함
         console.log("실행 대상 SQL : " + exec.sql);
 
-<<<<<<< HEAD
-=======
 
 //함수들 ======================================================================================//
 
@@ -415,7 +266,6 @@ var addUser = function(id, name, password, agreement, callback) {
 	
 	// 커넥션 풀에서 연결 객체를 가져옴
 	pool.getConnection(function(err, conn) {
->>>>>>> 22e642b8415732715978e77728ecdb1b7e32deec
         if (err) {
           console.log("SQL 실행 시 에러 발생함.");
           console.dir(err);
@@ -462,3 +312,31 @@ app.on("close", function () {
 http.createServer(app).listen(app.get("port"), function () {
   console.log("서버가 시작되었습니다. 포트 : " + app.get("port"));
 });
+
+//토큰 받기
+app.get('/api/users/auth', auth, (req, res) => {
+
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
+
+//로그아웃 토큰 지우기
+app.get('/api/users/logout', auth, (req, res) => {
+
+  User.findOneAndUpdate({ _id: req.user._id },
+    { token: "" }
+    , (err, user) => {
+      if (err) return res.json({ success: false, err });
+      return res.status(200).send({
+        success: true
+      })
+    })
+})
