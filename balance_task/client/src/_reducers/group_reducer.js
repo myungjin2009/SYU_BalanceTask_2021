53a4d3cf87b675acf3557781954ed952dfa55ed3
백정훈 <bjh9807@naver.com>
2021-11-17 17:14:53 +0900

    2234

--- balance_task/client/src/_reducers/group_reducer.js
+++ balance_task/client/src/_reducers/group_reducer.js
@@ -170,12 +170,13 @@ export default function reducer(state = initialState, action) {
       return state;
     }
     case END_PROJECT:{
+      console.log(action.payload);
       const {group, project_end_data} = action.payload;
       const new_obj = {
         group,
-        isComplete: project_end_data.isComplete
+        isComplete: project_end_data
       }
-      return {...state, group_end_data: [...new_obj]}
+      return {...state, group_end_data: [...state.group_end_data, new_obj]}
     }
     default:
       return state;
