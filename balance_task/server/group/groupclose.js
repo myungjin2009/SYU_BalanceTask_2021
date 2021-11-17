fe56f25126ed45855b9cbe6eaf43787f7ac6b279
백정훈 <bjh9807@naver.com>
2021-11-17 18:23:42 +0900

    6시24분

--- balance_task/server/group/groupclose.js
+++ balance_task/server/group/groupclose.js
@@ -5,15 +5,17 @@ const cookie = require("cookie");
 const { info } = require("console");
 var http = require('http');
 var url = require('url');
-
+const moment=require("moment");
 
 let groupclose= (req, res, next) => {
     console.log("groupend 함수 호출됨");
     let paramgroup_name=req.body.group;
-    let paramgroup_member=req.body.gruops_members;
+    let paramgroup_member=req.body.group_members;
     let paramleader=req.body.send_user_id;
 
-    for(i=0;i<=paramgroup_member.length;i++){
+    console.log(req.body);
+
+    for(i=0;i<paramgroup_member.length;i++){
       console.log(paramgroup_member[i].id);
       var member=paramgroup_member[i].id;
       const sql8="select count(aram_no) from aram"
@@ -21,8 +23,8 @@ let groupclose= (req, res, next) => {
         var no=rows[0]['count(aram_no)']+1;    
         var time=moment().format('YYYY-MM-DD HH:mm:ss');
 
-        var data = {aram_no:no, senduser:paramleader, receiveuser:member, group:paramgroup_name, sendtime:time, content:2, notsend:0 };
-
+        var data = {aram_no:no, senduser:paramleader, receiveuser:member, group:paramgroup_name, sendtime:time, content:2, notsend:1 };
+          console.log(data);
         const sql9 = "insert into aram set ?; ";
         //const sql2 = "SELECT * FROM vote; ";
         sql.pool.query(sql9,data, (err, rows, fields) => {
@@ -33,7 +35,7 @@ let groupclose= (req, res, next) => {
           
         console.log("aram 종료 문자 발신");
         }
-        next()
+        
         });//sql
       });  
     }
