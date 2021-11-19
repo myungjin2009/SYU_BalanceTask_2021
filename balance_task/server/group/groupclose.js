58bbcd8190e5aa826e0bbda5680b1764be22c644
백정훈 <bjh9807@naver.com>
2021-11-19 15:14:12 +0900

    22

--- balance_task/server/group/groupclose.js
+++ balance_task/server/group/groupclose.js
@@ -123,17 +123,17 @@ let groupclose= (req, res, next) => {
             
         }); 
 
-        // var sql8="update `groups` set group_no=(select group_no-1 from `groups` s1 where s1.group_no>(select group_no from `groups` s2 where s2.group_name='"+paramgroup_name+"') ) where group_no >(select group_no from `groups` s3 where s3.group_name='"+paramgroup_name+"')";
-        // sql.pool.query(sql8,(err,rows,fields)=>{
-        //   if (err) {
-        //       console.log(err);
-        //       } else { 
+        var sql8="update `groups` set `groups`.group_no=(select group_no-1 from `groups` as s1 where s1.group_no>(select group_no from `groups` as s2 where s2.group_name='"+paramgroup_name+"') ) where `groups`.group_no >(select group_no from `groups` as s3 where s3.group_name='"+paramgroup_name+"');";
+        sql.pool.query(sql8,(err,rows,fields)=>{
+          if (err) {
+              console.log(err);
+              } else { 
            
-        //       console.log("groups "+paramgroup_name+" 수정");
-        //       }
-        //       req.groupname=paramgroup_name;
-        //     next();
-        // }); 
+              console.log("groups "+paramgroup_name+" 수정");
+              }
+              req.groupname=paramgroup_name;
+            next();
+        }); 
 };
 
 module.exports= {groupclose};
