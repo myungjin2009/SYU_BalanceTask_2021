261556fbfa5ed7122c9f34c764a95834e8d382f7
백정훈 <bjh9807@naver.com>
2021-11-22 20:48:37 +0900

    0848

--- balance_task/server/groupboard/boardupdate.js
+++ balance_task/server/groupboard/boardupdate.js
@@ -6,6 +6,7 @@ const bcrypt=require('bcrypt');
 const moment = require("moment");
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart();
+const multer = require("multer");
 
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
@@ -25,36 +26,52 @@ var storage = multer.diskStorage({
   const upload = multer({ storage: storage });
 
 let boardupdate= (req, res, next) => {
-    console.log("groupboard  update함수 호출됨");
+    console.log("================================groupboardupdate함수 호출됨=======================================");
     console.log(req.body);
-
-    let paramcategory=req.body.category;
-    let urlgroup=req.body.group;
-    let paramId=req.body.id;
-    let paramtitle="게시물";
+    //console.log("req.files:"+req.files);
+    
+    let urlgroup=req.body.dataToSubmit.group;
+    let paramId=req.body.dataToSubmit.id;
+    let paramcategory=req.body.dataToSubmit.category;
+    let paramtitle=req.bodydataToSubmit.photo_name;
     let paramimages=[];
-    let paramtext=req.body.content;
-    //받아야 할값
-    let paramdate=req.body.date;
+    let paramcontent=req.body.dataToSubmit.content;
+    let paramkind=req.body.dataToSubmit.kind;    //받아야 할값
+    let paramdate=req.body.dataToSubmit.date;
     //let paramdate=req.body.board_number;
     for(i=0;i<req.files.length;i++){
         paramimages.push(`/image/${req.files[i].filename}`);
+        //console.log("req.files[i].filename:"+req.files[i].filename);
     }
-    // console.log( paramimages);
-    // if(paramimages==null){
-    //   paramimages=req.body.image;
-    // }
+    //console.log("paramimages :"+paramimages);
+    if(paramimages==null){
+      paramimages=req.body.image;
+    }
     
     //var sql1="update `groups` set (group_name,category,startdate,deadline,highlight,host,manager,content,group_images)=('"+paramgroup_name+"','"+paramcategory+"',"+paramstartdate+","+paramdeadline+",'"+paramhighlight+"','"+paramhost+"','"+parammanger+"','"+paramcontent+"','"+paramgroup_images+"') where group_name='"+paramgroup_name+"');"
-    var sql1="update groupboard set title'"+paramtitle+"',category='"+paramcategory+"',text='"+paramtext+"',image='"+paramimages+"',date='"+paramdate+"' where board_number='"+req.body.board_number+"';"
+    if(paramkind=='timeLine'){
+    var sql1="update groupboard set title'"+paramtitle+"',category='"+paramcategory+"',text='"+paramcontent+"',image='"+paramimages+"',date='"+paramdate+"' where board_number='"+req.body.board_number+"' and info_groupname='"+urlgroup+"';"
+      sql.pool.query(sql1,(err,rows,fields)=>{
+          if (err) {
+              console.log(err);
+            } else {
+              console.log("성공");
 
-    sql.pool.query(sql1,(err,rows,fields)=>{
-        if (err) {
-            console.log(err);
-          } else {
-            console.log("성공");
+            }
+      })   
+  }
 
-          }
-    })   
+  if(paramkind=='notice'){
+    var sql1="update groupnotice set title'"+paramtitle+"',category='"+paramcategory+"',text='"+paramcontent+"',image='"+paramimages+"',date='"+paramdate+"' where board_number='"+req.body.board_number+"' and info_groupname='"+urlgroup+"';"
+      sql.pool.query(sql1,(err,rows,fields)=>{
+          if (err) {
+              console.log(err);
+            } else {
+              console.log("성공");
+
+            }
+      })   
+  }
+    
     next();
 }
