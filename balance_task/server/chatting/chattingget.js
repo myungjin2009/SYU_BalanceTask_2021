c7e21c8557bd4446cf4ac52fc0c0063147d83657
백정훈 <bjh9807@naver.com>
2021-11-23 22:32:11 +0900

    1030

--- balance_task/server/chatting/chattingget.js
+++ balance_task/server/chatting/chattingget.js
@@ -17,34 +17,36 @@ let chattingget = (req, res, next) => {
     console.log("chatting 내역 호출");
     const sql2="select * from chat, user where user.id=chat.chat_id chat_id='"+paramid+"' and group_name='"+paramgroup+"' order by chat_no";
     sql.pool.query(sql2,(err,rows,fields)=>{
-        rows.forEach((info) => {
-            req.no=info.chat_no;
-            req.msg=info.msg;                //메시지
-            req.date=info.chat_date;         //보낸 날짜
-            req.id=info.chat_id;            //보낸 사람
-            req.name=info.name;             //보낸 사람
-            req.profile=info.user_image;    //프로필사진
-            req.group=info.group_name;      //그룹네임
-            req.my=false;
-            if(req.id===req.body.id){
-                req.my=true;
-            }
-            array.push({
-                message:req.msg,
-                user_name:req.group_name,
-                user_id:req.id,
-                group:req.group,
-                date:req.date,
-                my:req.my,
-                profile:req.profile,
-                id:req.no
+        if(rows==undefined){
+            return;
+        }else{
+            rows.forEach((info) => {
+                req.no=info.chat_no;
+                req.msg=info.msg;                //메시지
+                req.date=info.chat_date;         //보낸 날짜
+                req.id=info.chat_id;            //보낸 사람
+                req.name=info.name;             //보낸 사람
+                req.profile=info.user_image;    //프로필사진
+                req.group=info.group_name;      //그룹네임
+                req.my=false;
+                if(req.id===req.body.id){
+                    req.my=true;
+                }
+                array.push({
+                    message:req.msg,
+                    user_name:req.group_name,
+                    user_id:req.id,
+                    group:req.group,
+                    date:req.date,
+                    my:req.my,
+                    profile:req.profile,
+                    id:req.no
+                });
+                
             });
-            
-        });
+        }
         req.array=array;
-    
-          // socket.join(room);
-          // chat.to(room).emit('rMsg', data);
+        
         next();
     });
     
