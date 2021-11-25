a8e7901c8908252cb098b7011d75e4cfca8b4898
백정훈 <bjh9807@naver.com>
2021-11-25 12:02:19 +0900

    1202

--- balance_task/server/index.js
+++ balance_task/server/index.js
@@ -50,6 +50,15 @@ const io = require("socket.io")(httpServer, {
     origins: "*:*",
     methods: ["GET", "POST"],
   },
+  handlePreflightRequest: (req, res) => {
+    const headers = {
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+        "Access-Control-Allow-Credentials": true
+    };
+    res.writeHead(200, headers);
+    res.end();
+}
 });
 
 // const httpServer = require("http").createServer();
