6b53d44ea59d8178b444f885b770b6d91cdb3b92
백정훈 <bjh9807@naver.com>
2021-11-25 12:31:36 +0900

    1231

--- balance_task/client/src/components/views/GroupChatPage/GroupChat.js
+++ balance_task/client/src/components/views/GroupChatPage/GroupChat.js
@@ -11,7 +11,7 @@ import ChatBlock from "./ChatBlock";
 
 
 const GroupChat = (props) => {
-  const socket = io.connect("http://localhost:80");
+  const socket = io.connect("*");
   const {match:{params:{group}}} = props;
 
   const container_ref = useRef(null);
